546e55d85cdf8e5f2065fd8a7b9d310d751c22e6
Annabel Lau <annabel.n.lau@gmail.com>
2018-03-08 17:36:40 -0500

    plays video on click

--- components/film.js
+++ components/film.js
@@ -2,15 +2,34 @@ import React, { Component } from 'react'
 import {
     Video,
     View,
-    asset
+    asset,
+    MediaPlayerState,
+    VrButton
 } from 'react-vr'
 
-function Film() {
-    return (
-        <View style={{ margin: 0.2, height: 2 }}>
-            <Video style={{ height: 2.7 }} source={asset('onboarding_video.mp4')} />
-        </View>
-    )
+class Film extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            playerState: new MediaPlayerState({ autoPlay: false, muted: true })
+        }
+    }
+    render() {
+        console.log(this.state.playerState, "HALP")
+        return (
+            <View style={{ margin: 0.2, height: 2 }}>
+                <VrButton
+                    onClick={() => this.state.playerState.play()}>
+                    <Video
+                        style={{ height: 2.7 }}
+                        source={asset('onboarding_video.mp4')}
+                        playerState={this.state.playerState}
+                    />
+                </VrButton>
+            </View>
+        )
+
+    }
 }
 
 module.exports = Film
